6b8be603a140382cffc8b4b47ea64ac298f47e06
Luis <luisinserra@yahoo.com.br>
2017-08-15 19:48:53 -0300

    alertas colocados

--- www/js/index.js
+++ www/js/index.js
@@ -57,7 +57,7 @@ function onFileSystemSuccess(fs) {
     alert("Sucesso");
     var pathInicial=fs.root.fullPath;
     alert("Entrando com "+pathInicial+"...");
-    fs.root.fullPath = 'file:///storage/sdcard1';
+    fs.root.fullPath = 'file:///mnt/sdcard/';
     alert("mudou o path...");
     var dirReader = fs.root.createReader();
     alert("reader criado");
